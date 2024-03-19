f8d889726163bd63239142f27f3283e84ef11d63
Alex D <a21aledelfel@inspedralbes.cat>
2024-03-19 10:51:26 +0100

    Correcion Footer

---
+++ front/store/pinia.js
@@ -0,0 +1,9 @@
+export const useSesionCompraStore = defineStore('sesionCompra', () => {
+    const id_sesion_actual = ref(0)
+    
+    /*function increment() {
+      count.value++
+    }*/
+  
+    return { id_sesion_actual }
+  })
