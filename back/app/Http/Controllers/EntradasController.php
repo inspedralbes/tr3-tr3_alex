54da77a3e606017f5859c857c2064487c8ee4793
Alex D <a21aledelfel@inspedralbes.cat>
2024-04-10 10:54:27 +0200

    Envia entrada

--- back/app/Http/Controllers/EntradasController.php
+++ back/app/Http/Controllers/EntradasController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use App\Models\Entradas;
 use App\Models\Sessions;
 use App\Mail\enviarCorreo;
+use Illuminate\Support\Facades\Mail;
 
 class EntradasController extends Controller
 {
@@ -18,17 +19,18 @@ class EntradasController extends Controller
     public function store(Request $request)
     {
         $entradasData = $request->json()->all();
-
+    
         foreach ($entradasData['asientos'] as $entrada) {
             $nuevaEntrada = Entradas::create([
                 'sesion_cine_id' => $entrada['sesion_cine_id'],
                 'Butaca' => $entrada['Butaca'],
                 'Fila' => $entrada['Fila'],
                 'Asiento' => $entrada['Asiento'],
-                'email'=>$entrada['email']
+                'email' => $entradasData['email'] // Acceder al correo electrónico desde $entradasData
             ]);
+            // Luego de guardar cada entrada, envía el correo electrónico
+            Mail::to($entradasData['email'])->send(new enviarCorreo());
         }
-
         return response()->json(['message' => 'Entradas guardadas correctamente'], 201);
     }
 
